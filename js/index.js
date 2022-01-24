3fa8f5bb7d0406e400c8a35497a8dc527cfacd40
l <codn3@outlook.com>
2022-01-24 22:28:56 +0000

    modal, title, fullDescription

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ dates.map((item) => {
 
   // HEADING
   const timelineItem = document.createElement('div')
-  timelineItem.innerText = 'timeline-item'
+  //timelineItem.innerText = 'timeline-item'
   timelineItem.appendChild(timeline)
   timeline.appendChild
   const timelineItemDiv = document.createElement('div')
@@ -32,10 +32,25 @@ dates.map((item) => {
   document.body.prepend(timelineItemH2);
   timelineItemDiv.appendChild(timelineItemH2)
 
-  const timelineItemDate = document.createElement('span')
-  timelineItemDate.className = 'timeline-item-date'
-  timelineItemDate.textContent = item.date
-  timelineItemDiv.appendChild(timelineItemDate)
+  //  const timelineItemDate = document.createElement('span')
+  //  timelineItemDate.className = 'timeline-item-date'
+  //  timelineItemDate.textContent = item.date
+  //  timelineItemDiv.appendChild(timelineItemDate)
+
+//   const timelineItemTitle = document.createElement('span')
+//   timelineItemTitle.className = 'timeline-item-title'
+//   timelineItemTitle.textContent = item.title
+//   timelineItemDiv.appendChild(timelineItemTitle)
+
+// // const Image = querySelector.('image')
+// //   Image.className = 'timeline-item-image'
+// //   Image.imageContent = item.image
+// //   timelineItemDiv.appendChild(Image)
+
+//   const img = document.createElement("img");
+//  img.src = "image";
+//  document.body.appendChild(img);
+  
 
   const timelineItemSummary = document.createElement('text')
   timelineItemSummary.className = 'timeline-item-summary'
@@ -47,13 +62,13 @@ dates.map((item) => {
   timelineItemMoreInfo.textContent = 'more info'
   timelineItemMoreInfo.addEventListener("click", () => openModal(item));
   timelineItemDiv.appendChild(timelineItemMoreInfo)
-
+ 
 })
 
 // The Modal
 
 function openModal(item) {
-  console.log('Hi')
+  console.log('Bakery')
     const modal = document.createElement('div')
   modal.id = 'modal-container' ;
  
@@ -61,8 +76,38 @@ function openModal(item) {
   const date = document.createElement('h3') ;
      date.id = 'modal-date' ;
   date.textContent = item.date ;
-   console.log(date)
+  console.log(date)
   modal.appendChild(date) ;
 timeline.appendChild(modal)
+
+const title = document.createElement('h3') ;
+title.id = 'modal-title' ;
+title.textContent = item.title ;
+console.log(title)
+modal.appendChild(title) ;
+timeline.appendChild(modal) ;
+
+// const image = document.createElement('h3') ; 
+// image.id = 'modal-image' ;
+// //image.setAttribute("src", image);
+// img src = "image" ;
+// //image.imageContent = item.image ;
+// console.log(image)
+// modal.appendChild(image) ;
+// timeline.appendChild(modal) ;
+
+// const img = document.createElement("img");
+// img.src = "image";
+// document.body.appendChild(img);
+
+const fullDescription = document.createElement('p') ;
+fullDescription.id = 'modal-full-description' ;
+fullDescription.textContent = item.fullDescription ;
+console.log(fullDescription)
+modal.appendChild(fullDescription) ;
+timeline.appendChild(modal) ;
+
+
+
 }
 
